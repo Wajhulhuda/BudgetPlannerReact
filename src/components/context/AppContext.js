dfe32c4eeef443f38f743d0243a065037cf25634
Wajhul Huda <62687753+Wajhulhuda@users.noreply.github.com>
2023-03-03 09:02:15 +0530

    Add files via upload
    "Completed"

---
+++ src/components/context/AppContext.js
@@ -0,0 +1,54 @@
+import { useReducer } from "react";
+import { createContext } from "react";
+
+const initialState = {
+  budget: 2000,
+  expenses: [
+    { id: 12, name: "shopping", cost: 40 },
+    { id: 13, name: "holiday", cost: 400 },
+    { id: 14, name: "car service", cost: 50 },
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_ITEM":
+      // const newExpenses = state.expenses.push({id:15,name:action.payload.name,cost:action.payload.cost});
+      let newState = { ...state };
+      newState.expenses.push({
+        id: 15,
+        name: action.payload.name,
+        cost: parseInt(action.payload.cost),
+      });
+
+      return newState;
+    case "DELETE_ITEM":
+      let newState2 = { ...state };
+      // eslint-disable-next-line
+      newState2["expenses"] = newState2.expenses.filter((item) => {
+        if (item.id !== action.payload) return item;
+      });
+
+      return newState2;
+    default:
+      return state;
+  }
+}
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider
+      value={{
+        budget: state.budget,
+        expenses: state.expenses,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
+};
